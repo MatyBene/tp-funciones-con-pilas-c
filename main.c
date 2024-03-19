cf6310a894fb631720adf7efb8ced7a05ddc9230
MatyBene <benedettimaty@gmail.com>
2024-03-19 00:05:43 -0300

    agrega funcion 2

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <time.h>
 #include "pila.h"
 
-void cargarPilaRandom(Pila *p);
+void cargarPila(Pila *p);
+void pasarElementos(Pila p, Pila *q);
 
 int main()
 {
@@ -30,60 +31,89 @@ int main()
         printf("Ingrese opcion numerica\n");
         fflush(stdin);
         scanf("%d", &opcion);
-        printf("Desea salir: s/n");
-        fflush(stdin);
-        scanf("%c", &control);
         system("cls");
 
         switch(opcion){
             case 1:
+                printf("1. Hacer una funcion que permita ingresar varios elementos a una pila, tanto como quiera el usuario.\n\n");
+
+                cargarPila(&dada);
+
+                mostrar(&dada);
+
                 break;
             case 2:
+                printf("2. Hacer una funcion que pase todos los elementos de una pila a otra.\n\n");
+
+                Pila aux;
+                inicpila(&aux);
+
+                pasarElementos(dada, &aux);
+
+                printf("DADA");
+                mostrar(&dada);
+                printf("AUX");
+                mostrar(&aux);
                 break;
             case 3:
+                printf("3. Hacer una funcion que pase todos los elementos de una pila a otra, pero conservando el orden.\n\n");
                 break;
             case 4:
+                printf("4. Hacer una funcion que encuentre el menor elemento de una pila y lo retorna. La misma debe eliminar ese dato de la pila.\n\n");
                 break;
             case 5:
+                printf("5. Hacer una funcion que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada. Usar la funcion del ejercicio 4. (Ordenamiento por selección)\n\n");
                 break;
             case 6:
+                printf("6. Hacer una funcion que inserta en una pila ordenada un nuevo elemento, conservando el orden de esta.\n\n");
                 break;
             case 7:
+                printf("7. Hacer una funcion que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada. Usar la funcion del ejercicio 6.  (Ordenamiento por inserción)\n\n");
                 break;
             case 8:
+                printf("8. Hacer una funcion que sume y retorne los dos primeros elementos de una pila (tope y anterior), sin alterar su contenido.\n\n");
                 break;
             case 9:
+                printf("9. Hacer una funcion que calcule el promedio de los elementos de una pila, para ello hacer tambien una funcion que calcule la suma, otra para la cuenta y otra que divida. En total son cuatro funciones, y la funcion que calcula el promedio invoca a las otras 3.\n\n");
                 break;
             case 10:
+                printf("10. Hacer una funcion que reciba una pila con numeros de un solo digito (es responsabilidad de quien usa el programa) y que transforme esos digitos en un numero decimal.\n\n");
                 break;
             case 11:
+                printf("Realizar una funcion que cargue la pila sin utilizar la funcion leer.\n\n");
                 break;
             case 12:
+                printf("Realizar una funcion que muestre la pila sin utilizar la funcion mostrar.\n\n");
                 break;
             default:
                 printf("La opcion ingresada no es valida.");
         }
-
-    } while(control=='s');
-
-    cargarPilaRandom(&dada);
-
-    mostrar(&dada);
+        printf("\nDesea salir: s/n\n");
+        fflush(stdin);
+        scanf("%c", &control);
+        system("cls");
+    } while(control == 'n' || control == 'N');
 
     return 0;
 }
 
-void cargarPilaRandom(Pila *p){
+void cargarPila(Pila *p){
     char control;
 
     do
     {
         leer(p);
 
-        printf("\nDesea continuar? s/n");
+        printf("\nDesea continuar? s/n\n");
         fflush (stdin);
         scanf("%c", &control);
 
     }
     while (control=='s');
+}
+
+void pasarElementos(Pila p, Pila *q){
+    while(!pilavacia(&p)){
+        apilar(q, desapilar(&p));
+    }
 }
